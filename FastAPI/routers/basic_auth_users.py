a8d9d7ec9dc45b9d3f0f66f4ac8e61ee2df73f68
Sergio <sergio.fernandez.casasola.work@outlook.com>
2024-06-12 15:00:57 +0200

    Basic auth_user

--- FastAPI/routers/basic_auth_users.py
+++ FastAPI/routers/basic_auth_users.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 
-app = FastAPI()
+router = APIRouter(prefix="/basicauth",
+                tags=["basicauth"],
+                responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -49,7 +51,7 @@ async def current_user(token: str = Depends(oauth2)):
     user = search_user_db(token)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciales de autentificación inválidas", 
             headers={"WWW-Authenticate":"Bearer"}
         )
@@ -62,7 +64,7 @@ async def current_user(token: str = Depends(oauth2)):
     return user
 
 
-@app.post("/login")
+@router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
 
@@ -75,6 +77,6 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     
     return {"access_token": user.username, "token_type": "bearer"}
 
-@app.get("/users/me")
+@router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
